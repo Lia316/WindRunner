e88d9a5f62f1191faa5d17e00466c9e7ae743c73
Lia316 <kjk0825@postech.ac.kr>
2023-03-10 03:11:03 +0900

    feat: draw ground

--- OpenGL/Wind_runner.cpp
+++ OpenGL/Wind_runner.cpp
@@ -1,11 +1,10 @@
 #include <windows.h> 
 #include <gl/gl.h> 
-#include <GL/freeglut.h>
+#include <gl/glut.h>
 
-void init(void);
-void display(void);
+void init();
+void drawGround();
 void reshape(int w, int h);
-void keyboard(unsigned char key, int x, int y);
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
@@ -14,9 +13,51 @@ int main(int argc, char** argv) {
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Wind Runner");
 	init();
-	glutDisplayFunc(display);
+	glutDisplayFunc(drawGround);
 	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
 	glutMainLoop();
 	return 0;
+}
+
+typedef struct rect {
+	float x;
+	float y;
+	float width;
+	float height;
+} rect;
+rect rectangle;
+
+void init(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_FLAT);
+	rectangle.x = 0.;
+	rectangle.y = 0;
+	rectangle.width = glutGet(GLUT_WINDOW_WIDTH);
+	rectangle.height = glutGet(GLUT_WINDOW_HEIGHT)/4;
+}
+
+void drawGround()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.7f, 0.5f, 0.3f);
+	glBegin(GL_POLYGON);
+	glVertex2f(rectangle.x, rectangle.y);
+	glVertex2f(rectangle.x + rectangle.width, rectangle.y);
+	glVertex2f(rectangle.x + rectangle.width, rectangle.y + rectangle.height);
+	glVertex2f(rectangle.x, rectangle.y + rectangle.height);
+	glEnd();
+	glutSwapBuffers();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, w, 0, h, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	rectangle.width = w;
+	rectangle.height = h / 4;
 }
