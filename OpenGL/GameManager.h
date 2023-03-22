8fb204521bd0ec4b4e2a6c4eaa90a43ba7319d6e
Lia316 <kjk0825@postech.ac.kr>
2023-03-22 15:51:50 +0900

    refactor: separate gamemanager class from main

---
+++ OpenGL/GameManager.h
@@ -0,0 +1,37 @@
+#pragma once
+#include<string>
+#include "Entity.h"
+#include "Ground.h"
+#include "Hole.h"
+#include "Character.h"
+#include "Fire.h"
+#include "Star.h"
+
+#define MAXFIRE 4
+#define MAXSTAR 10
+
+class GameManager {
+public:
+	GameManager();
+	void draw();
+	void move(void(*t)(int));
+	void reshape(int w, int h);
+	void holemaker(void(*t)(int));
+	void firemaker(void(*t)(int));
+	void starmaker(void(*t)(int));
+	void keyboard(unsigned char key, int x, int y);
+	bool detectCollision(Entity* character, Entity* object);
+	bool detectSink(Entity* character, Entity* hole);
+	void showText(float x, float y, std::string string);
+
+	Ground* ground;
+	Hole* hole;
+	Character* character;
+	Fire* fire[MAXFIRE];
+	Star* star[MAXSTAR];
+
+	bool isGameEnd;
+	int score;
+	int firenum;
+	int starnum;
+};
